8604eb5e22a34278653ede9d27262b8fcd89a91f
Lucas <lcs.grc@outlook.com>
2021-08-09 23:41:01 +0200

    Add the profile picture feature within the whole app Fixes #1 - ongoing

--- apps/groupomania/src/app/profil/profil.component.ts
+++ apps/groupomania/src/app/profil/profil.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { FileUpload } from "primeng/fileupload";
 
 @Component({
-  selector: 'groupomania-profil',
-  templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.scss']
+    selector: "groupomania-profil",
+    templateUrl: "./profil.component.html",
+    styleUrls: ["./profil.component.scss"],
 })
 export class ProfilComponent implements OnInit {
+    private fileUpload!: FileUpload;
+    public imageBlob!: SafeUrl;
 
-  constructor() { }
+    constructor(private sanitize: DomSanitizer) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    public appendImage(image: { files: File[] }, fileUpload: FileUpload): void {
+        this.fileUpload = fileUpload;
+        this.imageBlob = this.sanitize.bypassSecurityTrustUrl(window.URL.createObjectURL(image.files[0]));
+        console.log(this.imageBlob);
+    }
 }
