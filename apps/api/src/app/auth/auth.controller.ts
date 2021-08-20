9c48a7ed47411fa9815ba153eabd83d9b3078bc2
Lucas <lcs.grc@outlook.com>
2021-08-20 16:46:22 +0200

    refactor: :building_construction: move the and decorators within the shared folder

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -2,25 +2,23 @@ import { Body, Controller, Get, Post, Req } from "@nestjs/common";
 import { CreateUserDto, SigninUserDto } from "libs/dto/src/lib/dto";
 import { Request } from "express";
 import { AuthService } from "./auth.service";
-import { Public } from "./decorators/public.decorator";
+import { Public } from "../shared/decorators/public.decorator";
 
+@Public()
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Public()
     @Post("signup")
     public async signup(@Body() user: CreateUserDto) {
         return await this.authService.signup(user);
     }
 
-    @Public()
     @Post("signin")
     public async signin(@Body() signinUser: SigninUserDto) {
         return await this.authService.signin(signinUser);
     }
 
-    @Public()
     @Get("refresh-access-token")
     public refreshAccessToken(@Req() req: Request) {
         return this.authService.refreshAccessToken(req);
